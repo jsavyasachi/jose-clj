fbc6ae24dfa1baf666a2211af25b5052a98dc35c
Savyasachi <jsavyasachi@gmail.com>
2026-07-09 19:19:23 -0700

    chore(build): populate pom description, url, and scm connection metadata
    Clojars listed this lib with no description and null homepage because
    b/write-pom emitted neither. Add :description and project :url to
    :pom-data and :connection/:developerConnection to :scm so Clojars and
    cljdoc get complete metadata. Release v0.1.2.

--- build.clj
+++ build.clj
@@ -8,7 +8,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'net.clojars.savya/jose-clj)
-(def version "0.1.1")
+(def version "0.1.2")
 (def class-dir "target/classes")
 (def basis (delay (b/create-basis {:project "deps.edn"})))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
@@ -24,8 +24,12 @@
                 :basis @basis
                 :src-dirs ["src"]
                 :scm {:url "https://github.com/jsavyasachi/jose-clj"
+                      :connection "scm:git:https://github.com/jsavyasachi/jose-clj.git"
+                      :developerConnection "scm:git:ssh://git@github.com/jsavyasachi/jose-clj.git"
                       :tag (str "v" version)}
-                :pom-data [[:licenses
+                :pom-data [[:description "Idiomatic Clojure JOSE: JWS, JWE, JWK/JWKS, and JWT (signed, encrypted, nested) over Nimbus JOSE+JWT."]
+                           [:url "https://github.com/jsavyasachi/jose-clj"]
+                           [:licenses
                             [:license
                              [:name "Eclipse Public License 2.0"]
                              [:url "https://www.eclipse.org/legal/epl-2.0/"]
